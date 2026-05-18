18dd07948e702129c9c60abcd70119e5bf88154d
Edi <gholipourhadi86@gmail.com>
2026-05-18 09:17:32 -0700

    Update rpaskal.java

--- rpaskal.java
+++ rpaskal.java
@@ -1,3 +1,13 @@
+/*
+Copyright 2026 Edi (Hadi Gholipour _ AxGooD)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+*/
+
 
 import java.awt.image.AreaAveragingScaleFilter;
 import java.util.Scanner;
